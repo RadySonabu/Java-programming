6325c0b073c5ed37b1265b955b674d09bc505324
RadySonabu <ardynubanos@yahoo.com>
2019-01-01 21:42:38 +0800

    first commit

---
+++ DETECTION/car.py
@@ -0,0 +1,20 @@
+import cv2
+camera = cv2.VideoCapture('E:/dspfinal/quiz/car2.mp4')
+car_cascade = cv2.CascadeClassifier('E:/dspfinal/quiz/cars.xml')
+
+
+while True:
+    
+    (ret,frame) = camera.read()
+    if ret is True:
+        grayvideo = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        cars = car_cascade.detectMultiScale(grayvideo, 1.1, 1)
+    else:
+        continue
+    for (x,y,w,h) in cars:
+     cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2)
+     cv2.imshow("video",frame)
+    if cv2.waitKey(1)== ord('q'):
+        break
+camera.release()
+cv2.destroyAllWindows()
---
+++ DETECTION/cat.py
@@ -0,0 +1,22 @@
+import cv2
+
+
+camera = cv2.VideoCapture (0)
+car_cascade = cv2.CascadeClassifier('C:/darknet/opencv/build/etc/haarcascades/haarcascade_frontalcatface_extended.xml')
+while True:
+    
+    (ret,frame) = camera.read()
+    if ret is True:
+         grayvideo = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+         cars = car_cascade.detectMultiScale(grayvideo, 1.1, 1)
+    else:
+        continue
+
+    
+    for (x,y,w,h) in cars:
+     cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2)
+     cv2.imshow("video",frame)
+    if cv2.waitKey(1)== ord('q'):
+        break
+camera.release()
+cv2.destroyAllWindows()
